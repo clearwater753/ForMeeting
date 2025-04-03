b9649a7862d2f703eb5828170c6e71d5fba4b539
单章辉 <ci8278489@163.com>
2025-04-03 11:20:27 +0800

    学习python自定义包并修改gitignore

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-class Student(object):
-    @property
-    def birth(self):
-        return self._birth
-
-    @birth.setter
-    def birth(self, value):
-        self._birth = value
-
-    @property
-    def age(self):
-        return 2015 - self._birth
-    
-s = Student()
-print(s.birth)
+from my_package.sub_package.hello import print_hello
+print_hello()
